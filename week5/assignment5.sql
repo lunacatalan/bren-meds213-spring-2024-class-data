48f759ebceecb4ff4b2aa2cb20c804f094b21f49
lunacatalan <lunahcatalan@gmail.com>
2024-05-07 15:13:31 -0700

    uploaded assignment 5

--- week5/assignment5.sql
+++ week5/assignment5.sql
@@ -1,6 +1,35 @@
 -- to open do sqlite3 then .open file_name.sqlite
 .table
 
+
+-- BASH Essentials
+-- 1. The outputs of these three commands are the same. This is because the ls command asks what files and subdirectories are in the week3 folder,
+--    and the . directs the question to the current directory. Since we are in the week3 directory, the outputs of those commands are the same. For
+--    ls  "$(pwd)/../week3", this is saying print the current working directory, then navigate to the parent directory, and enter the week3 subdirectory,
+--    resulting in the same answer as the other two commands. 
+
+-- 2. The cat command displays the combined output of all the csv files (*.csv), and then the | is used as a pipe operator, and it counts the number of lines. 
+--    No filenames are shown because the cat *.csv combined all of the csv's so there are no file names by the time the lines are counted.
+
+-- 3. The result is `100 species.csv`. This means it only displays the lines in the species.csv file. Because we specified the filename, it only displayed the
+--    the lines from that csv. 
+
+-- 4. You can change the variable name: 
+name=Moe
+echo $name'_Howard'
+
+-- 5. When running bash myscript.sh *.csv, the variables are the different .csv files. It sees multiple arguments passed to it because its iterating through the 
+--     csv files. 
+
+-- 6. bash myscript.sh "$(date)" $(date) The value of variable $3 is the third date variable needed for the script. 
+
+-- 7. When we ran the `sort junk_file.txt > junk_file.txt` it deleted everything in the junk_fil.txt file. 
+--      Created a new file junk_fil_2.txt with the sorted output with: sort junk_file.txt > junk_fil_2.txt
+--      Then I moved the contents of junk_fil_2.txt to the junk_file.txt by using this command: 
+--      mv junk_fil_2.txt ~/Documents/dev/eds213/bren-meds213-spring-2024-class-data/week3/junk_file.txt
+
+-- 8. If there is a space between the * and the .csv, then it will delete everything in the current directly because the * means everything. 
+
 SELECT * FROM Bird_eggs WHERE Nest_ID = '14eabaage01';
 
 -- PART 1
@@ -55,6 +84,38 @@ INSERT INTO Bird_eggs
 
 SELECT * FROM Bird_eggs WHERE Nest_ID = '14eabaage01';
 
+
+-- not_in
+SELECT Code
+    FROM Species
+    WHERE Code NOT IN (SELECT DISTINCT Species FROM Bird_nests);
+
+bash test_harness.sh not_in 1000 'SELECT Code
+    FROM Species
+    WHERE Code NOT IN (SELECT DISTINCT Species FROM Bird_nests);' \
+     database.db part_2_timings.csv
+
+-- outer
+SELECT Code
+    FROM Bird_nests RIGHT JOIN Species
+    ON Species = Code
+    WHERE Nest_ID IS NULL;
+
+bash test_harness.sh outer_query 1000 'SELECT Code
+    FROM Bird_nests RIGHT JOIN Species
+    ON Species = Code
+    WHERE Nest_ID IS NULL;' \
+     database.db part_2_timings.csv
+
+-- set
+SELECT Code FROM Species
+EXCEPT
+SELECT DISTINCT Species FROM Bird_nests;
+
+bash test_harness.sh set_query 1000 'SELECT Code FROM Species
+EXCEPT
+SELECT DISTINCT Species FROM Bird_nests;' \
+     database.db part_2_timings.csv
 
 
 
--- week5/test_harness.sh
+++ week5/test_harness.sh
@@ -9,3 +9,29 @@
       #db_file:  database file
      # csv_file: CSV file to create or append to
     
+# inputs 
+label="$1"
+num_reps="$2"
+query="$3"
+db_file="$4"
+csv_file="$5"
+
+# record a start time
+start_time=$(date +%s)
+
+# iterate through the number of reps
+for i in $(seq $num_reps); do
+    duckdb "$db_file" -c "$query"
+done
+
+# record end time
+end_time=$(date +%s)
+
+# calculate the total time
+elapsed=$((end_time - start_time))
+
+# use -l to get more decimal places
+rep_time=$(bc -l <<< "$elapsed/$num_reps")
+
+# output the lavel and elapsed time
+echo "$label","$rep_time" >> "$csv_file"
